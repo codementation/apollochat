0925239525e3cfa23ef37b72f30b9061880283f1
Codementation, LLC <support@codementation.com>
2021-03-04 15:35:49 -0600

    adds error handling and validation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,42 +1,72 @@
-const { User } = require('../models');
 const bcrypt = require('bcrypt');
+const { UserInputError } = require('apollo-server')
+
+const { User } = require('../models')
 
 module.exports = {
   Query: {
     getUsers: async () => {
       try {
-        const users = await User.findAll();
+        const users = await User.findAll()
 
-        return users;
+        return users
       } catch (err) {
-        console.log(err);
+        console.log(err)
       }
     },
   },
   Mutation: {
-    register: async (parent, args, context, info) => {
-      let { username, email, password, confirmPassword } = args;
+    register: async (_, args) => {
+      let { username, email, password, confirmPassword } = args
+      let errors = {}
+
       try {
         // Validate input data
+        if (email.trim() === '') errors.email = 'email must not be empty'
+        if (username.trim() === '')
+          errors.username = 'username must not be empty'
+        if (password.trim() === '')
+          errors.password = 'password must not be empty'
+        if (confirmPassword.trim() === '')
+          errors.confirmPassword = 'repeat password must not be empty'
 
-        // Verify username/email
+        if (password !== confirmPassword)
+          errors.confirmPassword = 'passwords must match'
 
+        // // Check if username / email exists
+        // const userByUsername = await User.findOne({ where: { username } })
+        // const userByEmail = await User.findOne({ where: { email } })
 
-          // Hash password
-          password = await bcrypt.hash(password, 6)
+        // if (userByUsername) errors.username = 'Username is taken'
+        // if (userByEmail) errors.email = 'Email is taken'
+
+        if (Object.keys(errors).length > 0) {
+          throw errors
+        }
+
+        // Hash password
+        password = await bcrypt.hash(password, 6)
 
         // Create user
         const user = await User.create({
           username,
           email,
           password,
-        });
+        })
+
         // Return user
-        return user;
+        return user
       } catch (err) {
-        console.log(err);
-        throw err;
+        console.log(err)
+        if (err.name === 'SequelizeUniqueConstraintError') {
+          err.errors.forEach(
+            (e) => (errors[e.path] = `${e.path} is already taken`)
+          )
+        } else if (err.name === 'SequelizeValidationError') {
+          err.errors.forEach((e) => (errors[e.path] = e.message))
+        }
+        throw new UserInputError('Bad input', { errors })
       }
     },
   },
-};
+}
