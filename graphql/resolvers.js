9ca87a2ffc072ff14e4af449ee80a4b0360278db
Codementation, LLC <support@codementation.com>
2021-03-04 14:41:05 -0600

    adds password encryption

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,15 +1,42 @@
-const { User } = require('../models')
+const { User } = require('../models');
+const bcrypt = require('bcrypt');
 
 module.exports = {
   Query: {
     getUsers: async () => {
       try {
-        const users = await User.findAll()
+        const users = await User.findAll();
 
-        return users
+        return users;
       } catch (err) {
-        console.log(err)
+        console.log(err);
       }
     },
   },
-}
+  Mutation: {
+    register: async (parent, args, context, info) => {
+      let { username, email, password, confirmPassword } = args;
+      try {
+        // Validate input data
+
+        // Verify username/email
+
+
+          // Hash password
+          password = await bcrypt.hash(password, 6)
+
+        // Create user
+        const user = await User.create({
+          username,
+          email,
+          password,
+        });
+        // Return user
+        return user;
+      } catch (err) {
+        console.log(err);
+        throw err;
+      }
+    },
+  },
+};
