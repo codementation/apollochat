c191f6c2b594db90f353a7b4ede142e28baa8bc3
Codementation, LLC <support@codementation.com>
2021-03-04 22:34:57 -0600

    adds login model and typeDefs

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,72 +1,115 @@
-const bcrypt = require('bcrypt');
-const { UserInputError } = require('apollo-server')
+const bcrypt = require('bcrypt')
+const { UserInputError, AuthenticationError } = require('apollo-server')
 
 const { User } = require('../models')
 
 module.exports = {
-  Query: {
-    getUsers: async () => {
-      try {
-        const users = await User.findAll()
+    Query: {
+        getUsers: async () => {
+            try {
+                const users = await User.findAll()
 
-        return users
-      } catch (err) {
-        console.log(err)
-      }
+                return users
+            } catch (err) {
+                console.log(err)
+            }
+        },
+
+        login: async (_, args) => {
+            const { username, password } = args
+            let errors = {}
+
+            try {
+                if (username.trim() === '')
+                    errors.username = 'username must not be empty'
+                if (password === '')
+                    errors.password = 'password must not be empty'
+
+                if (Object.keys(errors).length > 0) {
+                    throw new UserInputError('bad input', { errors })
+                }
+
+                const user = await User.findOne({
+                    where: { username },
+                })
+
+                if (!user) {
+                    errors.username = 'user not found'
+                    throw new UserInputError('user not found', { errors })
+                }
+
+                const correctPassword = await bcrypt.compare(
+                    password,
+                    user.password,
+                )
+
+                if (!correctPassword) {
+                    errors.password = 'password is incorrect'
+                    throw new AuthenticationError('password is incorrect', {
+                        errors,
+                    })
+                }
+
+                return user
+            } catch (err) {
+                console.log(err)
+                throw err
+            }
+        },
     },
-  },
-  Mutation: {
-    register: async (_, args) => {
-      let { username, email, password, confirmPassword } = args
-      let errors = {}
+    Mutation: {
+        register: async (_, args) => {
+            let { username, email, password, confirmPassword } = args
+            let errors = {}
 
-      try {
-        // Validate input data
-        if (email.trim() === '') errors.email = 'email must not be empty'
-        if (username.trim() === '')
-          errors.username = 'username must not be empty'
-        if (password.trim() === '')
-          errors.password = 'password must not be empty'
-        if (confirmPassword.trim() === '')
-          errors.confirmPassword = 'repeat password must not be empty'
+            try {
+                // Validate input data
+                if (email.trim() === '')
+                    errors.email = 'email must not be empty'
+                if (username.trim() === '')
+                    errors.username = 'username must not be empty'
+                if (password.trim() === '')
+                    errors.password = 'password must not be empty'
+                if (confirmPassword.trim() === '')
+                    errors.confirmPassword = 'repeat password must not be empty'
 
-        if (password !== confirmPassword)
-          errors.confirmPassword = 'passwords must match'
+                if (password !== confirmPassword)
+                    errors.confirmPassword = 'passwords must match'
 
-        // // Check if username / email exists
-        // const userByUsername = await User.findOne({ where: { username } })
-        // const userByEmail = await User.findOne({ where: { email } })
+                // // Check if username / email exists
+                // const userByUsername = await User.findOne({ where: { username } })
+                // const userByEmail = await User.findOne({ where: { email } })
 
-        // if (userByUsername) errors.username = 'Username is taken'
-        // if (userByEmail) errors.email = 'Email is taken'
+                // if (userByUsername) errors.username = 'Username is taken'
+                // if (userByEmail) errors.email = 'Email is taken'
 
-        if (Object.keys(errors).length > 0) {
-          throw errors
-        }
+                if (Object.keys(errors).length > 0) {
+                    throw errors
+                }
 
-        // Hash password
-        password = await bcrypt.hash(password, 6)
+                // Hash password
+                password = await bcrypt.hash(password, 6)
 
-        // Create user
-        const user = await User.create({
-          username,
-          email,
-          password,
-        })
+                // Create user
+                const user = await User.create({
+                    username,
+                    email,
+                    password,
+                })
 
-        // Return user
-        return user
-      } catch (err) {
-        console.log(err)
-        if (err.name === 'SequelizeUniqueConstraintError') {
-          err.errors.forEach(
-            (e) => (errors[e.path] = `${e.path} is already taken`)
-          )
-        } else if (err.name === 'SequelizeValidationError') {
-          err.errors.forEach((e) => (errors[e.path] = e.message))
-        }
-        throw new UserInputError('Bad input', { errors })
-      }
+                // Return user
+                return user
+            } catch (err) {
+                console.log(err)
+                if (err.name === 'SequelizeUniqueConstraintError') {
+                    err.errors.forEach(
+                        (e) => (errors[e.path] = `${e.path} is already taken`),
+                    )
+                } else if (err.name === 'SequelizeValidationError') {
+                    err.errors.forEach((e) => (errors[e.path] = e.message))
+                }
+                throw new UserInputError('Bad input', { errors })
+            }
+        },
     },
-  },
 }
